d57f50dd469bf53fe1ca111e22f950b8cff15094
Denis Iakovlev <denis@deniss-air.speedport.ip>
2024-01-09 22:41:58 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
-import logo from './logo.svg';
+import dishwasher from './dishwasher.jpeg'
 import './App.css';
+import { useState } from 'react';
+import ReactCodeInput from 'react-code-input'
+ 
+const numbers = "1234567";
+
+
+const width = '40px'
+const height = '50px'
+const fontSize = '30px'
+const props = {
+  inputStyle: {
+    fontFamily: 'monospace',
+    margin:  '4px',
+    MozAppearance: 'textfield',
+    width,
+    borderRadius: '3px',
+    fontSize,
+    height,
+    paddingLeft: '7px',
+    backgroundColor: 'black',
+    color: 'lightskyblue',
+    border: '1px solid lightskyblue'
+  },
+  inputStyleInvalid: {
+    fontFamily: 'monospace',
+    margin:  '4px',
+    MozAppearance: 'textfield',
+    width,
+    borderRadius: '3px',
+    fontSize,
+    height,
+    paddingLeft: '7px',
+    backgroundColor: 'black',
+    color: 'red',
+    border: '1px solid red'
+  }
+}
+
+
 
 function App() {
+  const [curNum, setNum] = useState("");
+  const [showResult, setShowResult] = useState(false);
+
+  function check(v) {
+    if (v === numbers) {
+      setShowResult(true);
+    } else {
+      setShowResult(false);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!showResult &&
+        <ReactCodeInput type='number' fields={numbers.length} {...props} value={curNum} onChange={v => {
+          setNum(v);
+          check(v);
+        }} />
+        }
+        {showResult &&
+          <img src={dishwasher} alt="dishwasher" />
+        }
+        
       </header>
     </div>
   );
